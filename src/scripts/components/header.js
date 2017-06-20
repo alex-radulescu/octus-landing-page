325f27c70cdcbb4ecea27f811f15b7072c7c3fc8
Alex Radulescu <alex.radulescu@octus.com.sg>
2017-06-21 07:27:23 +0800

    0.6

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -6,6 +6,13 @@ class Header extends View {
 	constructor(el) {
 		super(el);
 
+		const section = document.querySelectorAll(".section");
+		this.sections = {};
+
+		Array.prototype.forEach.call(section, e => {
+			this.sections[e.id] = e.offsetTop;
+		});
+
 		this.onWindowScroll();
 		//this.addListener("click", ".burger-menu", this.toggleMobileMenu.bind(this));
 		this.addListener("scroll", document, this.onWindowScroll.bind(this));
@@ -18,13 +25,25 @@ class Header extends View {
 		} else {
 			this.el.classList.add("is-invisible")
 		}
+
+		const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+		let i = 0;
+		for (i in this.sections) {
+			if (this.sections[i] <= scrollPosition) {
+				if (document.querySelector('.main-navigation-link.is-active')) {
+					document.querySelector('.main-navigation-link.is-active').classList.remove("is-active");
+				}
+				document.querySelector('.main-navigation-link[href*=' + i + ']').classList.add('is-active');
+			}
+		}
 	}
 
 	navigate(e) {
 		e.preventDefault();
 		jump(e.target.getAttribute("href"));
-		document.querySelectorAll(".main-navigation-link").forEach(item =>  item.classList.remove("is-active"));
-		e.target.classList.add("is-active");
+		// document.querySelectorAll(".main-navigation-link").forEach(item =>  item.classList.remove("is-active"));
+		// e.target.classList.add("is-active");
 	}
 
 	static init(selector = ".header", base = document.body) {
